1251f79aae99d203e936cc413421bb1d3a0cebb8
rachier_alal <rachiermark285@gmail.com>
2023-08-15 01:35:47 +0300

    react tic-tac-toe

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from 'react';
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  } 
+  
+  return (
+    <React.Fragment>
+          <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick}  />
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
+        
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Square( {value, onSquareClick} ) {
+
+
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
